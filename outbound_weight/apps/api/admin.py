d7a6cd6b8609fcbc779dd7332a77d631ede29062
Alexander Tyapkin <atyapkin20@gmail.com>
2018-01-29 13:59:52 +0200

    changes

--- outbound_weight/apps/api/admin.py
+++ outbound_weight/apps/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Calculator
+
+
+class CalculatorAdmin(admin.ModelAdmin):
+    list_display = ['unit_weight', 'longest_side', 'medium_side', 'smallest_side',
+                    'size_tier', 'order_date']
+
+
+admin.site.register(Calculator, CalculatorAdmin)
--- outbound_weight/settings.py
+++ outbound_weight/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'outbound_weight.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fba_calculator',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

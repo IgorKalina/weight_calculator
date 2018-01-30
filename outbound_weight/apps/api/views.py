d0e39bd126b990eb87553f5002be443a34c0072b
Alexander Tyapkin <atyapkin20@gmail.com>
2018-01-30 17:25:33 +0200

    almost done

--- outbound_weight/apps/api/views.py
+++ outbound_weight/apps/api/views.py
@@ -1,5 +1,5 @@
 import json
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.generic.base import View
 from datetime import datetime
 
@@ -7,6 +7,7 @@ from outbound_weight.apps.api.api import FBACalculatorAPI
 from outbound_weight.apps.api.models import Calculator
 
 from .constants import ORDER_DATE_FMT
+from .utils import parse_str_to_list
 
 
 class CalculateOutboundData(View):
@@ -40,20 +41,38 @@ class BulkCalculateOutboundData(View):
     """
 
     def get(self, request):
-        request_data = dict(
-            unit_weight=request.GET.get('unit_weight'),
-            medium_side=request.GET.get('medium_side'),
-            longest_side=request.GET.get('longest_side'),
-            smallest_side=request.GET.get('smallest_side'),
-            size_tier=request.GET.get('size_tier'),
-            order_date=request.GET.get('order_date')
-        )
-        if Calculator.objects.filter(**request_data).exists():
-            instance = Calculator.objects.get(**request_data)
-            return {'weight': instance.weight, 'period': instance.period}
-        api = FBACalculatorAPI(**request_data)
-        weight = api.get_outbound_shipping_weight()
-        period = api.get_calculations_period()
-        request_data.update(weight=weight, period=period)
-        Calculator.objects.create(**request_data)
-        return HttpResponse({'calculations_result': ca}, content='application/json')
+        request_data = dict()
+        calculation_result = dict()
+        if request.GET.get('list_of_items'):
+            list_of_items = parse_str_to_list(request.GET.get('list_of_items'))
+            for item in list_of_items:
+                print '>>>>', item
+                if Calculator.objects.filter(**item).exists():
+                    print 'exists'
+                    # TODO: modify to return the json with items
+                    instance = Calculator.objects.get(**item)
+                    return JsonResponse(json.dumps({'weight': str(instance.weight),
+                                                    'period': instance.period,
+                                                    'asin': instance.asin}),
+                                        content_type='application/json')
+                request_data[item['asin']] = dict(
+                    unit_weight=item.get('unit_weight'),
+                    longest_side=item.get('longest_side'),
+                    medium_side=item.get('medium_side'),
+                    smallest_side=item.get('smallest_side'),
+                    size_tier=item.get('size_tier'),
+                    order_date=datetime.strptime(item.get('order_date'), ORDER_DATE_FMT)
+                )
+        if request_data:
+            for asin, item in request_data.items():
+                api = FBACalculatorAPI(**item)
+                calculation_result[asin] = dict(
+                    weight=str(api.get_outbound_shipping_weight()),
+                    period=api.get_calculations_period()
+                )
+                item.update(calculation_result.get(asin))
+                Calculator.objects.update_or_create(asin=asin, defaults=item)
+                print calculation_result
+            return JsonResponse({'calculation_result': calculation_result}, content_type='application/json')
+
+        return JsonResponse({'calculations_result': 'No_result'}, content_type='application/json')
